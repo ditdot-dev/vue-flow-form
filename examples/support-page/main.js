d26a50d4232aef98929ba30f3ba9cbba9c7e2bec
EkaterinaVu <ekaterina.vujasinovic@gmail.com>
2020-09-13 19:07:30 +0200

    Added support page example folder

---
+++ examples/support-page/main.js
@@ -0,0 +1,12 @@
+/*
+  Copyright (c) 2020 - present, DITDOT Ltd. - MIT Licence
+  https://github.com/ditdot-dev/vue-flow-form
+  https://www.ditdot.hr/en
+*/
+
+import Vue from 'vue'
+import Example from './Example.vue'
+
+new Vue({
+  render: h => h(Example)
+}).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,8 @@ module.exports = {
     index: {
       // Replace with your .js entry file path. 
       // To see the quiz example, use 'examples/quiz/main.js'
-      entry: 'examples/questionnaire/main.js',
+      // To see the support page example, use 'examples/support-page/main.js'
+      entry: 'examples/support-page/main.js',
       template: 'public/index.html',
       filename: 'index.html'
     }
