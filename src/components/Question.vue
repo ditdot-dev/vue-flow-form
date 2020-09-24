4f4ebb01366063b1606c56fcf5c4567660c9e68a
EkaterinaVu <ekaterina.vujasinovic@gmail.com>
2020-09-24 12:48:18 +0200

    Dropdown Ok button fixed

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -189,6 +189,11 @@
         const q = this.$refs.questionComponent
 
         if (q) {
+          if (this.question.type === QuestionType.Dropdown) {
+            this.$emit('answer', q)
+            return
+          } 
+
           if (!q.focused) {
             this.$emit('answer', q)
           }
