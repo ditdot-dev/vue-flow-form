2e63a8149b03f1fc2eaf79828317b14b07f3d283
EkaterinaVu <ekaterina.vujasinovic@gmail.com>
2020-10-11 21:33:13 +0200

    Test cleanup

--- tests/unit/vff-test.spec.js
+++ tests/unit/vff-test.spec.js
@@ -1,21 +1,9 @@
 import { mount, shallowMount } from '@vue/test-utils'
 import Example from '@/examples/questionnaire/Example.vue'
-import QuestionModel, { QuestionType, LinkOption } from '@/src/models/QuestionModel.js'
+import QuestionModel, { QuestionType, ChoiceOption} from '@/src/models/QuestionModel.js'
 import FlowForm from '@/src/components/FlowForm.vue'
 import FlowFormQuestion from '@/src/components/Question.vue'
-import LanguageModel from '@/src/models/LanguageModel'
-import FlowFormDropdownType from '@/src/components/QuestionTypes/DropdownType.vue'
-import FlowFormEmailType from '@/src/components/QuestionTypes/EmailType.vue'
-import FlowFormLongTextType from '@/src/components/QuestionTypes/LongTextType.vue'
 import FlowFormMultipleChoiceType from '@/src/components/QuestionTypes/MultipleChoiceType.vue'
-import FlowFormNumberType from '@/src/components/QuestionTypes/NumberType.vue'
-import FlowFormPasswordType from '@/src/components/QuestionTypes/PasswordType.vue'
-import FlowFormPhoneType from '@/src/components/QuestionTypes/PhoneType.vue'
-import FlowFormSectionBreakType from '@/src/components/QuestionTypes/SectionBreakType.vue'
-import FlowFormTextType from '@/src/components/QuestionTypes/TextType.vue'
-import FlowFormUrlType from '@/src/components/QuestionTypes/UrlType.vue'
-import { IsMobile } from '@/src/mixins/IsMobile'
-  
 
 const factory = (values = {}) => {
   return shallowMount(Example, {
@@ -27,75 +15,115 @@ const factory = (values = {}) => {
   })
 }
 
+const deepFactory = (values = {}) => {
+  return mount(Example, {
+    data () {
+      return {
+        ...values
+      }
+    }
+  })
+}
+
+const wrapper = deepFactory({
+                    questions: [
+                        new QuestionModel({
+                            title: 'Question',
+                            type: QuestionType.MultipleChoice,
+                            options: [
+                                new ChoiceOption({
+                                    label: 'Answer'
+                                })
+                            ]
+                        })
+                    ]
+                })
+
 describe('Example', () => {
+
+    let form 
+    let question
+    let multipleChoice
+
+    (async function getComponent() {
+        await wrapper.vm.$nextTick()
+        form = wrapper.findComponent(FlowForm)
+        question = wrapper.findComponent(FlowForm).findAllComponents(FlowFormQuestion).at(0)
+        multipleChoice = wrapper.findComponent(FlowForm).findComponent(FlowFormQuestion).findComponent(FlowFormMultipleChoiceType)  
+    })()
+
+
     it('Example is a component', () => {
+
         const wrapper = factory()
 
         expect(wrapper.findComponent(Example).vm)
     })
 
-     it('Example contains a FlowForm component', () => {
-        const wrapper = mount(Example)
+    it('contains a FlowForm component', () => {
+         
+        const wrapper = factory()
 
         expect(wrapper.findComponent(FlowForm).exists()).toBe(true)
      })
     
-    it('Example contains a completed and submitted data', () => {
-        const wrapper = mount(Example)
+    
+    it('renders a submit button on the final screen', () => {
 
-        expect(wrapper.vm.completed).toBe(false)
-        expect(wrapper.vm.submitted).toBe(false)
-     })
+        const wrapper = deepFactory()
+    
+        expect(wrapper.find(".o-btn-action").text()).toBe("Submit")
+    })
 
 
-    it("onSendData sets the submitted data to true", () => {
-        const wrapper = factory({ submitted: false })
-        wrapper.vm.onSendData()
+    it("removes submit button and renders a success message on submit button click", () => {
+
+        const wrapper = deepFactory()
+        const submitButton = wrapper.find('.f-submit button')
+        submitButton.trigger('click')
             
         expect(wrapper.vm.submitted).toBe(true)
+        setTimeout(() => {
+            expect(form.find('.text-success').exists()).toBe(true)
+            expect(form.classes('.f-submit')).toBe(false)
+        }, 100)
     })
 
-    it("Shows text success message when submitted", () => {
-         const wrapper = mount(Example, {
-             data() {
-                return {
-                    submitted: true
-                }
-            }
+
+    it("renders a MultipleChoice question type with one option", () => {
+         
+        wrapper.vm.$nextTick(() => {
+            expect(question.exists()).toBe(true)
+            expect(form.findAllComponents(FlowFormQuestion)).toHaveLength(1)
+            expect(question.find('.f-text').text()).toEqual("Question")
+            expect(question.find('.f-enter').exists()).toBe(false)
+            expect(multipleChoice.exists()).toBe(true)
+            expect(multipleChoice.find('.f-radios').findAll('li')).toHaveLength(1)
+            expect(multipleChoice.find('.f-key').exists()).toBe(true)
+            expect(multipleChoice.find('.f-key').text()).toEqual("A")
+            expect(multipleChoice.find('.f-label').exists()).toBe(true)
+            expect(multipleChoice.find('.f-label').text()).toEqual("Answer")
+            expect(multipleChoice.classes('.f-multiple')).toBe(false)
+            expect(multipleChoice.classes('.f-selected')).toBe(false)
         })
-           
-        expect(wrapper.findComponent(FlowForm).find('.text-success').exists()).toBeTruthy()
+         
     })
 
-    it('There is a submit button on the final screen', () => {
-        const wrapper = mount(Example)
-    
-        expect(wrapper.find("button").text()).toBe("Submit")
-    
-    })
-
-    it("Question component exists", () => {
-         const wrapper = mount(Example, {
-             data() {
-                return {
-                    questions: [
-                         new QuestionModel({
-                            id: 'first_name',
-                            tagline: 'Hi! Welcome to our demo survey 😊',
-                            title: 'What is your first name?',
-                            type: QuestionType.Text,
-                            required: true,
-                            placeholder: 'Start typing here...'
-                        })
-                    ]
-                }
-            }
+     it("allows to select an answer and click OK", () => {
+         
+         wrapper.vm.$nextTick(async () => {
+            const option = multipleChoice.findAll('.f-radios li').at(0)
+            await option.trigger('click')
+             
+            expect(form.find('.f-enter').exists()).toBe(true)
+            expect(multipleChoice.find('.f-selected').exists()).toBe(true)
+             
+            const okButton = question.find('.o-btn-action')
+            await okButton.trigger('click')
+            
+            expect(question.emitted().answer).toBeTruthy()
         })
-           
-    wrapper.vm.$nextTick(() => {
-        expect(wrapper.findComponent(FlowForm).findComponent(FlowFormQuestion).exists()).toBeTruthy()
-        expect(wrapper.findComponent(FlowForm).findAllComponents(FlowFormQuestion)).toHaveLength(1)
-  })
-    })
-    
+         
+     })
+     
 })
