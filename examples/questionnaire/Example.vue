4383fde62c5cca47fad93cd315c324fdd7ab4152
kygwang <kygwang@users.noreply.github.com>
2020-10-30 00:11:32 -0400

    modified first question, added name in place of logo, and trying out different theme-green

--- examples/questionnaire/Example.vue
+++ examples/questionnaire/Example.vue
@@ -1,16 +1,11 @@
 // Create and setup your form here
- 
+
 <template>
   <div>
     <header class="vff-header">
       <div class="f-container">
        <!-- Add custom logo here -->
-        <svg class="f-logo" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMid meet" viewBox="0 0 35.606 11.211">
-          <path d="M.134.837H3.21V8.01h4.203v2.18H.134V.837z"/>
-          <path d="M11.875.59c1.48 0 2.668.448 3.567 1.344s1.35 2.052 1.35 3.47c0 1.48-.445 2.7-1.336 3.632S13.38 10.45 11.9 10.45c-1.678 0-2.954-.54-3.827-1.622-.717-.9-1.08-2.022-1.09-3.397-.01-1.36.39-2.484 1.193-3.374C9.06 1.08 10.292.59 11.875.59zm0 2.283c-.563 0-1.003.222-1.323.662-.338.467-.507 1.124-.507 1.972 0 .865.162 1.524.487 1.978a1.58 1.58 0 0 0 1.369.682c.588 0 1.04-.223 1.355-.668s.474-1.07.474-1.875c0-1.834-.62-2.75-1.855-2.75z"/>
-          <path d="M21.565 7.078V5.055h4.217v5.163h-1.986l-.13-.908c-.693.76-1.617 1.142-2.777 1.142-1.383 0-2.488-.437-3.313-1.3s-1.243-2.03-1.252-3.464c-.01-1.462.385-2.65 1.18-3.567.875-1.012 2.11-1.518 3.7-1.518 1.21 0 2.207.303 3 .907s1.264 1.457 1.447 2.556h-2.92c-.207-.787-.73-1.182-1.57-1.182-.553 0-.988.236-1.303.707s-.475 1.153-.475 2.043c0 1.695.652 2.542 1.96 2.542.363 0 .695-.103.998-.306a1.29 1.29 0 0 0 .572-.784h-1.35v.002z"/>
-          <path d="M30.556.59c1.48 0 2.668.448 3.568 1.344s1.348 2.052 1.348 3.47c0 1.48-.443 2.7-1.336 3.632S32.06 10.45 30.58 10.45c-1.678 0-2.953-.54-3.826-1.622-.72-.892-1.082-2.022-1.1-3.398-.008-1.357.39-2.48 1.193-3.372C27.74 1.08 28.974.59 30.556.59zm0 2.283c-.563 0-1.002.222-1.322.662-.336.467-.506 1.125-.506 1.972 0 .865.162 1.524.486 1.978s.78.682 1.37.682 1.04-.223 1.355-.668.475-1.07.475-1.875c-.002-1.834-.62-2.75-1.857-2.75z"/>
-        </svg>
+        <h3> Retirement Calculator</h3>
       </div>
     </header>
 
@@ -33,13 +28,13 @@
             </span>
           </p>
           <p class="f-description">Note: No data will be saved and/or sent in this demo.</p>
-        </div>  
+        </div>
       </template>
 
       <!-- We've overriden the default "completeButton" slot content -->
       <template v-slot:completeButton>
         <div class="f-submit" v-if="!submitted">
-          <button 
+          <button
             class="o-btn-action"
             ref="button"
             type="submit"
@@ -90,7 +85,7 @@
           new QuestionModel({
             id: 'first_name',
             tagline: 'Hi! Welcome to our demo survey 😊',
-            title: 'What is your first name?',
+            title: 'What is your age?',
             type: QuestionType.Text,
             required: true,
             placeholder: 'Start typing here...'
@@ -250,7 +245,7 @@
         // completeButton slot.
         this.onSendData()
       },
-      
+
       onSendData() {
         // Set `submitted` to true so the form knows not to allow back/forward
         // navigation anymore.
@@ -298,7 +293,7 @@
 </script>
 
 <style lang="css">
-  @import '../../src/assets/css/themes/theme-minimal.css';
+  @import '../../src/assets/css/themes/theme-green.css';
   /* If using the npm package, use the following lines instead of the one above */
   /* @import '~@ditdot-dev/vue-flow-form/dist/vue-flow-form.css'; */
   /* @import '~@ditdot-dev/vue-flow-form/dist/vue-flow-form.theme-minimal.css'; */
