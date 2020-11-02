529102874f5d867d6c0494b9daa71e1b6b943b6f
kygwang <kygwang@users.noreply.github.com>
2020-11-02 14:07:16 -0500

    created a new file structure for the new quiz off of the sample questionnaire structure

--- vue.config.js
+++ vue.config.js
@@ -7,10 +7,10 @@ module.exports = {
   publicPath: '',
   pages: {
     index: {
-      // Replace with your .js entry file path. 
+      // Replace with your .js entry file path.
       // To see the quiz example, use 'examples/quiz/main.js'
       // To see the support page example, use 'examples/support-page/main.js'
-      entry: entry || 'examples/questionnaire/main.js',
+      entry: entry || 'examples/firstquiz/main.js',
       template: 'public/index.html',
       filename: 'index.html'
     }
