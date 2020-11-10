83ccd15bdbfe94335fc3bfe1297b7f16fc49c758
Sarmed <sarmedrizvi@gmail.com>
2020-11-11 03:07:04 +0500

    theme and fixes

--- src/components/QuestionTypes/SalaryType.vue
+++ src/components/QuestionTypes/SalaryType.vue
@@ -1,33 +1,52 @@
 <template>
-  <span class="d-flex justify-content-between w-100">
-    <span :style="{ position: 'absolute', color: Dollarcolor }">$</span>
-    <input
-      ref="input"
-      v-bind:type="inputType"
-      style="padding-left: 30px"
-      v-bind:value="dollar"
-      :disabled="question.checkbox"
-      v-bind:required="question.required"
-      v-on:keydown="onKeyDown"
-      v-on:keyup="onChange"
-      v-on:keyup.enter.prevent="onEnter"
-      v-on:keyup.tab.prevent="onEnter"
-      v-on:focus="setFocus"
-      @input="handleInput"
-      v-on:blur="unsetFocus"
-      v-bind:placeholder="placeholder"
-    />
+  <div class="d-flex justify-content-between w-100">
+    <template v-if="question.mask">
+      <span :style="{ position: 'absolute', color: Dollarcolor }">$</span>
+      <the-mask
+        ref="input"
+        v-bind:mask="question.mask"
+        v-bind:masked="false"
+        :disabled="question.checkbox"
+        style="padding-left: 20px"
+        v-bind:type="inputType"
+        v-bind:value="value"
+        v-bind:required="question.required"
+        v-on:keydown.native="onKeyDown"
+        v-on:keyup.native="onChange"
+        v-on:focus.native="setFocus"
+        v-on:blur.native="unsetFocus"
+        v-on:keyup.native.enter.prevent="onEnter"
+        v-on:keyup.native.tab.prevent="onEnter"
+        v-bind:placeholder="placeholder"
+      />
+    </template>
+    <template v-else>
+      <span :style="{ position: 'absolute', color: Dollarcolor }">$</span>
+      <input
+        ref="input"
+        v-bind:type="inputType"
+        v-bind:value="dollar"
+        :disabled="question.checkbox"
+        v-bind:required="question.required"
+        v-on:keydown="onKeyDown"
+        style="padding-left: 20px"
+        v-on:keyup="onChange"
+        v-on:keyup.enter.prevent="onEnter"
+        v-on:keyup.tab.prevent="onEnter"
+        v-on:focus="setFocus"
+        @input="handleInput"
+        v-on:blur="unsetFocus"
+        v-bind:placeholder="placeholder"
+      />
+    </template>
     <b-form-checkbox
-      style="font-size: 16px; width: 600px"
+      style="font-size: 16px"
       class="d-flex justify-content-center align-items-center vff-checkbox"
-      id="checkbox-1"
-      value="true"
       v-model="question.checkbox"
-      name="checkbox-1"
     >
-      <span class="">{{ question.checkboxText }}</span>
+      <span>{{ question.checkboxText }}</span>
     </b-form-checkbox>
-  </span>
+  </div>
 </template>
 
 <script>
@@ -43,6 +62,7 @@ import TheMask from "vue-the-mask/src/component";
 export default {
   extends: DollarType,
   name: QuestionType.Salary,
+
   components: {
     TheMask,
   },
@@ -51,7 +71,20 @@ export default {
   },
   methods: {
     isValid() {
-      return this.question.checkbox || this.dataValue;
+      if (!this.question.required) {
+        return true;
+      }
+      if (this.question.checkbox) {
+        return true;
+      }
+
+      if (
+        this.question.mask &&
+        this.dataValue.length !== this.question.mask.length
+      ) {
+        return false;
+      }
+      return true;
     },
   },
 };
