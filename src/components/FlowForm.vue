35c687f397e0eab3604b22fa331e4a981d983534
kygwang <kygwang@users.noreply.github.com>
2020-11-12 08:51:19 -0500

    added router link button to the Calculate Button to the Results page

--- src/components/FlowForm.vue
+++ src/components/FlowForm.vue
@@ -1,143 +1,85 @@
 // Form template and logic
 
 <template>
-  <div class="vff" :class="{ 'vff-not-standalone': !standalone }">
-    <div class="f-container">
-      <div class="f-form-wrap">
-        <flow-form-question
-          ref="questions"
-          v-for="(q, index) in questionList"
-          v-bind:question="q"
-          v-bind:language="language"
-          v-bind:key="'q' + index"
-          v-bind:active="q.index === activeQuestionIndex"
-          v-model="q.answer"
-          v-on:answer="onQuestionAnswered"
-          v-bind:reverse="reverse"
-          :noButton="
+<div class="vff" :class="{ 'vff-not-standalone': !standalone }">
+  <div class="f-container">
+    <div class="f-form-wrap">
+      <flow-form-question ref="questions" v-for="(q, index) in questionList" v-bind:question="q" v-bind:language="language" v-bind:key="'q' + index" v-bind:active="q.index === activeQuestionIndex" v-model="q.answer" v-on:answer="onQuestionAnswered"
+        v-bind:reverse="reverse" :noButton="
             questions.length === activeQuestionIndex + 1 &&
             activeQuestionComponent() &&
             activeQuestionComponent().showOkButton()
-          "
-        />
+          " />
 
-        <!-- Complete/Submit screen slots -->
-        <div
-          v-if="isOnLastStep"
-          class="vff-animate f-fade-in-up field-submittype"
-        >
-          <slot name="complete">
-            <!-- Default content for the "complete" slot -->
-            <div class="f-section-wrap">
-              <p>
-                <span class="fh2">{{ language.thankYouText }}</span>
-              </p>
-            </div>
-          </slot>
-
-          <slot name="completeButton">
-            <!-- Default content for the "completeButton" slot -->
-            <button
-              class="o-btn-action"
-              ref="button"
-              type="button"
-              href="#"
-              v-on:click.prevent="submit()"
-              v-if="!submitted"
-              v-bind:aria-label="language.ariaSubmitText"
-            >
-              <span>{{ language.submitText }}</span>
-            </button>
-            <a
-              class="f-enter-desc"
-              href="#"
-              v-on:click.prevent="submit()"
-              v-if="!submitted"
-              v-html="language.formatString(language.pressEnter)"
-            >
-            </a>
-            <p class="text-success" v-if="submitted">
-              {{ language.successText }}
+      <!-- Complete/Submit screen slots -->
+      <div v-if="isOnLastStep" class="vff-animate f-fade-in-up field-submittype">
+        <slot name="complete">
+          <!-- Default content for the "complete" slot -->
+          <div class="f-section-wrap">
+            <p>
+              <span class="fh2">{{ language.thankYouText }}</span>
             </p>
-          </slot>
-        </div>
+          </div>
+        </slot>
+
+        <slot name="completeButton">
+          <!-- Default content for the "completeButton" slot -->
+          <button class="o-btn-action" ref="button" type="button" href="#" v-on:click.prevent="submit()" v-if="!submitted" v-bind:aria-label="language.ariaSubmitText">
+            <span>{{ language.submitText }}</span>
+          </button>
+          <a class="f-enter-desc" href="#" v-on:click.prevent="submit()" v-if="!submitted" v-html="language.formatString(language.pressEnter)">
+          </a>
+          <p class="text-success" v-if="submitted">
+            {{ language.successText }}
+          </p>
+        </slot>
       </div>
     </div>
-    <div class="d-flex flex-column progress-cicles">
-      <div
-        :class="`d-flex justify-content-center progress-circle ${
+  </div>
+  <div class="d-flex flex-column progress-cicles">
+    <div :class="`d-flex justify-content-center progress-circle ${
           activeQuestionIndex === index && 'active'
-        }`"
-        v-for="(question, index) in questions"
-        :key="index"
-        @click="handleProgressBar(index)"
-      >
-        {{ index + 1 }}
-      </div>
+        }`" v-for="(question, index) in questions" :key="index" @click="handleProgressBar(index)">
+      {{ index + 1 }}
     </div>
-    <div class="vff-footer">
-      <div class="footer-center">
-        <div
-          v-if="
+  </div>
+  <div class="vff-footer">
+    <div class="footer-center">
+      <div v-if="
             questions.length === activeQuestionIndex + 1 &&
             activeQuestionComponent() &&
             activeQuestionComponent().showOkButton()
-          "
-        >
-          <button class="complete-button">Calculate</button>
-        </div>
-        <div v-else>
-          <div
-            v-if="progressbar"
-            class="f-progress"
-            v-bind:class="{
+          ">
+        <button class="complete-button">
+          <router-link to="/results">Calculate</router-link>
+        </button>
+      </div>
+      <div v-else>
+        <div v-if="progressbar" class="f-progress" v-bind:class="{
               'not-started': percentCompleted === 0,
               completed: percentCompleted === 100,
-            }"
-          >
-            <div class="f-nav">
-              <a
-                class="f-next"
-                href="#"
-                v-bind:class="{ 'f-disabled': !isNextQuestionAvailable() }"
-                v-on:click.prevent="goToNextQuestion()"
-                role="button"
-                v-bind:aria-label="language.ariaNext"
-              >
-                <svg
-                  version="1.1"
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlns:xlink="http://www.w3.org/1999/xlink"
-                  x="0px"
-                  y="0px"
-                  width="42.333px"
-                  height="28.334px"
-                  viewBox="78.833 5.5 42.333 28.334"
-                  aria-hidden="true"
-                >
-                  <path
-                    d="M117.963,8.031l-17.961,20.529L82.042,8.031l-2.041,1.784l18.98,21.695c0.258,0.295,0.629,0.463,1.02,0.463c0.39,0,0.764-0.168,1.02-0.463l18.98-21.695L117.963,8.031z"
-                  />
-                </svg>
-                <span class="f-nav-text" aria-hidden="true">{{
+            }">
+          <div class="f-nav">
+            <a class="f-next" href="#" v-bind:class="{ 'f-disabled': !isNextQuestionAvailable() }" v-on:click.prevent="goToNextQuestion()" role="button" v-bind:aria-label="language.ariaNext">
+              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="42.333px" height="28.334px" viewBox="78.833 5.5 42.333 28.334" aria-hidden="true">
+                <path d="M117.963,8.031l-17.961,20.529L82.042,8.031l-2.041,1.784l18.98,21.695c0.258,0.295,0.629,0.463,1.02,0.463c0.39,0,0.764-0.168,1.02-0.463l18.98-21.695L117.963,8.031z" />
+              </svg>
+              <span class="f-nav-text" aria-hidden="true">{{
                   language.next
                 }}</span>
-              </a>
-            </div>
-            <div class="f-progress-bar">
-              <div
-                class="f-progress-bar-inner"
-                v-bind:style="'width: ' + percentCompleted + '%;'"
-              ></div>
-            </div>
-            {{
+            </a>
+          </div>
+          <div class="f-progress-bar">
+            <div class="f-progress-bar-inner" v-bind:style="'width: ' + percentCompleted + '%;'"></div>
+          </div>
+          {{
               language.percentCompleted.replace(":percent", percentCompleted)
             }}
-          </div>
         </div>
       </div>
     </div>
   </div>
+</div>
 </template>
 
 <script>
@@ -330,8 +272,7 @@ export default {
     },
     onKeyUpListener(e) {
       if (
-        e.shiftKey ||
-        ["Tab", "Enter"].indexOf(e.key) === -1 ||
+        e.shiftKey || ["Tab", "Enter"].indexOf(e.key) === -1 ||
         this.submitted
       ) {
         return;
