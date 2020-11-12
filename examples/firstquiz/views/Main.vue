7ac104b4dc60af94206b8cbb6cac2fea28994788
Sarmed <sarmedrizvi@gmail.com>
2020-11-12 05:08:50 +0500

    some revisions

--- examples/firstquiz/views/Main.vue
+++ examples/firstquiz/views/Main.vue
@@ -1,30 +1,34 @@
 <template>
-<div>
-<header class="vff-header">
-  <div class="f-container row">
-    <span class="col-lg-6" style="display: flex; justify-content: flex-start">
-      <a style="font-size: 24px">Gig Retirement Calculator</a>
-    </span>
-    <span class="col-lg-6" style="display: flex; justify-content: flex-end">
-      <a href="https://www.gigfinance.org">
-      <img src="../../../src/assets/images/gigfinance_logo_white.svg" width="120px"/>
-      </a>
-    </span>
+  <div>
+    <header class="vff-header">
+      <div
+        class="d-flex justify-content-between align-items-center"
+        style="margin-left: 50px; margin-right: 50px"
+      >
+        <a style="font-size: 24px">Gig Retirement Calculator</a>
+
+        <a href="https://www.gigfinance.org">
+          <img
+            src="../../../src/assets/images/gigfinance_logo_white.svg"
+            width="120px"
+          />
+        </a>
+      </div>
+    </header>
+
+    <transition name="component-fade" mode="out-in">
+      <router-view
+    /></transition>
   </div>
-</header>
-
-<transition name="component-fade" mode="out-in">
-  <router-view/></transition>
-
-</div>
 </template>
 
 <script>
 </script>
 
 <style>
-.component-fade-enter-active, .component-fade-leave-active {
-  transition: opacity .3s ease;
+.component-fade-enter-active,
+.component-fade-leave-active {
+  transition: opacity 0.3s ease;
 }
 .component-fade-enter, .component-fade-leave-to
 /* .component-fade-leave-active below version 2.1.8 */ {
@@ -35,10 +39,9 @@
   max-width: 50%;
   height: auto;
   display: inline-block;
-  padding-top: .59375rem;
-  padding-bottom: .59375rem;
+  padding-top: 0.59375rem;
+  padding-bottom: 0.59375rem;
   margin-right: 1.125rem;
-  white-space: nowrap
+  white-space: nowrap;
 }
-
 </style>
