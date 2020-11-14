3c01fc21b5d66ba6582d4b294c19e5d6261ad6b9
Sarmed <sarmedrizvi@gmail.com>
2020-11-14 22:18:43 +0500

    minor changes

---
+++ src/store/index.js
@@ -0,0 +1,53 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+const userInformation = {
+  state: {
+    userInput: {}, // data captured in RetirementReferral.vue input form
+    incomeData: {}, // data formatted from the input for tax API
+    taxUpdate: {},
+    taxSummary: {} // tax API's output data to be displayed in Results.vue
+  },
+  mutations: {},
+  getters: {}
+};
+
+const calculatorDrag = {
+  state: {
+    postIraTaxData: {
+      taxAvoided: null,
+      taxAdvantageRatio: null,
+      personalContribution: null
+    }, // calculations to be displayed in RetirementOptions.vue
+    postSepIraTaxData: {
+      taxAvoided: null,
+      taxAdvantageRatio: null,
+      businessContribution: null
+    }, // calculations to be displayed in RetirementOptions.vue
+    postSimpleIraTaxData: {
+      taxAvoided: null,
+      taxAdvantageRatio: null,
+      personalContribution: null,
+      businessContribution: null
+    }, // calculations to be displayed in RetirementOptions.vue
+    postIndividual401kTaxData: {
+      taxAvoided: null,
+      taxAdvantageRatio: null,
+      personalContribution: null,
+      businessContribution: null
+    } // calculations to be displayed in RetirementOptions.vue
+  },
+  mutations: {},
+  getters: {}
+};
+
+const store = new Vuex.Store({
+  modules: {
+    userInformation,
+    calculatorDrag
+  }
+});
+
+export default store;
