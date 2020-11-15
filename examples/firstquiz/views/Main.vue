3a08c271a69d623bb116070409d8f4ab396f3519
kygwang <kygwang@users.noreply.github.com>
2020-11-15 12:48:36 -0500

    bolded the header

--- examples/firstquiz/views/Main.vue
+++ examples/firstquiz/views/Main.vue
@@ -1,25 +1,19 @@
 <template>
-  <div>
-    <header class="vff-header">
-      <div
-        class="d-flex justify-content-between align-items-center"
-        style="margin-left: 50px; margin-right: 50px"
-      >
-        <a style="font-size: 24px">Gig Retirement Calculator</a>
+<div>
+  <header class="vff-header">
+    <div class="d-flex justify-content-between align-items-center" style="margin-left: 50px; margin-right: 50px">
+      <a style="font-size: 24px; font-weight: 600">Gig Retirement Calculator</a>
 
-        <a href="https://www.gigfinance.org">
-          <img
-            src="../../../src/assets/images/gigfinance_logo_white.svg"
-            width="120px"
-          />
-        </a>
-      </div>
-    </header>
+      <a href="https://www.gigfinance.org">
+        <img src="../../../src/assets/images/gigfinance_logo_white.svg" width="120px" />
+      </a>
+    </div>
+  </header>
 
-    <transition name="component-fade" mode="out-in">
-      <router-view
-    /></transition>
-  </div>
+  <transition name="component-fade" mode="out-in">
+    <router-view />
+  </transition>
+</div>
 </template>
 
 <script>
@@ -30,8 +24,12 @@
 .component-fade-leave-active {
   transition: opacity 0.3s ease;
 }
-.component-fade-enter, .component-fade-leave-to
-/* .component-fade-leave-active below version 2.1.8 */ {
+
+.component-fade-enter,
+.component-fade-leave-to
+
+/* .component-fade-leave-active below version 2.1.8 */
+  {
   opacity: 0;
 }
 
