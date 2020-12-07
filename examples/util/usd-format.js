b47aa3981655f5878b59bbfb6f521362d535fc03
Sarmed <sarmedrizvi@gmail.com>
2020-12-08 03:16:17 +0500

    Updates

---
+++ examples/util/usd-format.js
@@ -0,0 +1,8 @@
+export const usdFormat = value => {
+  return value.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0
+  });
+};
