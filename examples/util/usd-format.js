312ea4685bfe0513831a0351bb89ac0dbd140d7b
Sarmed <sarmedrizvi@gmail.com>
2020-12-08 19:06:09 +0500

    salary issue fixed

--- examples/util/usd-format.js
+++ examples/util/usd-format.js
@@ -1,8 +1,16 @@
-export const usdFormat = value => {
-  return value.toLocaleString("en-US", {
+export const usdFormat = (value, fraction) => {
+  if (fraction === 0) {
+    return Number(value).toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0
+    });
+  }
+  return Number(value).toLocaleString("en-US", {
     style: "currency",
     currency: "USD",
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2
+    minimumFractionDigits: fraction || 2,
+    maximumFractionDigits: fraction || 2
   });
 };
