ee9a8d153387589d13f73419e65adec01cedf11d
Sarmed <sarmedrizvi@gmail.com>
2021-02-23 01:34:58 +0500

    user can see values now

---
+++ src/constants/env.js
@@ -0,0 +1,15 @@
+export const userInput = JSON.parse(localStorage.getItem("vuex"))
+  ?.userInformation?.userInput;
+  
+// example User Input
+// age: "45"
+// business_name: "soft"
+// dependents: "2"
+// employee_count: "lessthan100"
+// entity: "sCorporation"
+// expenses: "423234"
+// first_name: "sarmed"
+// income: "5423553"
+// salary: ""
+// tax_filing_state: "co"
+// tax_filing_status: "marriedFilingSeparately"
