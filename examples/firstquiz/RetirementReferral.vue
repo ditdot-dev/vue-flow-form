52ca630afb3c0b47eef319a2eacad1b8816e0219
Muhammad Salman Siddiqui <salmansidd991@gmail.com>
2021-03-25 00:02:01 +0500

    entry page working

--- examples/firstquiz/RetirementReferral.vue
+++ examples/firstquiz/RetirementReferral.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="hello"> 
     <router-view />
 
     <flow-form
--- examples/firstquiz/views/Intro.vue
+++ examples/firstquiz/views/Intro.vue
@@ -1,50 +1,53 @@
 <template>
-<div class="main">
-  <div class="content flex2">
-    <p class="heading">
-      Welcome to the Gig <br />
-      Retirement Calculator
-    </p>
-    <p class="para">
-      Fill out the questionnaire to calculate <br />
-      your 2020 profit after taxes. <br />
-      <br />
-
-      You'll need your last year's tax returns, and <br />
-      estimate how much you made and spent this tax year. <br />
-      <br />
-
-      Then, we will make a personalized<br />
-      retirement account suggestion <br />
-      to maximize your tax savings💰! <br />
-      <br />
-      <strong>Note:</strong> Your information is safe with us 😊. <br />
-      We will never sell your data.
-    </p>
-    <br /><br />
-    <router-link to="/input" aria-label="start input page"><button>GET STARTED</button></router-link>
+  <div>
+    <div class="intro_main">
+      <div class="intro_content_left">
+        <h1 class="intro_header">Welcome to the Gig Retirement Calculator</h1>
+        <div class="para">
+          <p>
+            Fill out the questionnaire to calculate your 2020 profit after
+            taxes.
+          </p>
+          <p>
+            We will make a personalized retirement account suggestion to
+            maximize your tax savings 💰!
+          </p>
+          <p>
+            You'll need your last year's tax returns, and estimate how much you
+            made and spent this tax year.
+          </p>
+          <h5>Let’s get started~</h5>
+        </div>
+      </div>
+      <div class="intro_content_right">
+        <img class="right_img" src="../../../src/assets/images/wallet.svg" />
+      </div>
+      <!-- <router-link to="/input" aria-label="start input page"
+      ><button>GET STARTED</button></router-link
+    > -->
+    </div>
+    <div class="arrow">
+      <img class="down_arrow_img" src="../../../src/assets/images/down_arrow.png" />
+    </div>
+    <div>
+      
+    </div>
   </div>
-  <div class="image">
-    <div class="img"></div>
-  </div>
-</div>
 </template>
 
 <script>
-// import "../../../src/assets/css/intro.css";
 export default {
   computed: {
     username() {
       // We will see what `params` is shortly
       return this.$route.params.username;
-    },
+    }
   },
   methods: {
     goBack() {
       window.history.length > 1 ? this.$router.go(-1) : this.$router.push("/");
-    },
-  },
+    }
+  }
 };
 </script>
 <style scoped src="../css/intro.css"></style>
-
