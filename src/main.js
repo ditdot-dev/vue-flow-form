5975a1614af1a1a3559bce008ff6e492a6f26326
dennis <922751+spinn@users.noreply.github.com>
2021-05-19 15:28:17 +0200

    Remove global component registration

--- src/main.js
+++ src/main.js
@@ -4,29 +4,6 @@ import Question from './components/Question.vue'
 import QuestionModel, { QuestionType, ChoiceOption, LinkOption, MaskPresets } from './models/QuestionModel'
 import LanguageModel from './models/LanguageModel'
 
-// Declare install function executed by Vue.use()
-export function install(Vue) {
-  if (install.installed) return
-  install.installed = true
-  Vue.component('FlowForm', FlowForm)
-}
-
-// Create module definition for Vue.use()
-const plugin = {
-  install
-}
-
-// Auto-install when vue is found (eg. in browser via <script> tag)
-let GlobalVue = null
-if (typeof window !== 'undefined') {
-  GlobalVue = window.Vue
-} else if (typeof global !== 'undefined') {
-  GlobalVue = global.Vue
-}
-if (GlobalVue) {
-  GlobalVue.use(plugin)
-}
-
 // To allow use as module (npm/webpack/etc.) export component
 export default FlowForm
 
