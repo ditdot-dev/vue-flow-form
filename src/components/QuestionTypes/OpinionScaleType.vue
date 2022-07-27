7149cdfbdd21307d24da084085cde1f949fef984
EkaterinaVu <ekaterina.vujasinovic@gmail.com>
2022-07-27 10:55:34 +0200

    Add rate icons styling

--- src/components/QuestionTypes/OpinionScaleType.vue
+++ src/components/QuestionTypes/OpinionScaleType.vue
@@ -15,7 +15,7 @@
         <div v-else-if="isIconScale" class="f-icon-wrap">
             <div class="f-icon">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/>
+                <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" stroke-width=".5"/>
               </svg>
             </div>
             <div class="f-key">{{ getToggleKey(option.value) }}</div>
